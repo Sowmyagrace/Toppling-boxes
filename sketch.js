b83c351794a9abebeb93bad338af52e17575db6f
Sowmyagrace <71786247+Sowmyagrace@users.noreply.github.com>
2020-10-13 22:22:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box1,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+  
+   
+   
+   box1 =new Box(200,300,50,50);
+   box2= new Box(230,100,50,100);
+   
+  ground=new Ground(200,395,400,20);
+
+   // console.log(ground.width);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+   // rectMode(CENTER);
+   // rect(ground.position.x,ground.position.y,400,20);
+  // console.log(box2.body.position.x);
+   // console.log(box2.body.position.y);
+  //  console.log(box2.body.angle);
+   box1.display();
+   
+   box2.display();
+   ground.display();
+}
